3d2fc90da64d2a309c6b54ee393b825814ec6860
andre silveira <andre.silveira@teamviewer.com>
2021-11-10 23:11:23 +0000

    add TextField component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import Button from 'components/Button'
 import Loader from 'components/Loader'
+import TextField from 'components/TextField'
 
 const Home = () => {
   return (
     <div>
+      <TextField />
       <Button size="medium">TESTE</Button>
     </div>
   )
